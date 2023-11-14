2e774bf6e6d1f72bca7a787b18711641983652f8
MilyaushaShamsutdinova <m.shamsutdinova@innopolis.university>
2023-11-14 12:23:15 +0300

    implemented northwest corner method

--- main.cpp
+++ main.cpp
@@ -2,16 +2,26 @@
 
 #include "headers/Matrix.hpp"
 #include "headers/Vector.hpp"
+#include "headers/NorthwestCornerMethod.hpp"
 
 using namespace std;
 
 void perform() {
     int n, m;
 
-    cout << "Enter number of basic variables:\n";
-    cin >> n;
-    cout << "Enter number of constraint functions:\n";
+    //cout << "Enter number of basic variables:\n";
     cin >> m;
+    //cout << "Enter number of constraint functions:\n";
+    cin >> n;
+
+    Vector S(m), D(n);
+    Matrix C(m, n);
+
+    cin >> C;
+    cin >> S;
+    cin >> D;
+
+    NorthwestCornerMethod::start_method(C,S,D);
 
     //rest of the code
 
@@ -19,6 +29,6 @@ void perform() {
 
 int main() {
     perform();
-    system("pause");
+    //system("pause");
     return EXIT_SUCCESS;
 }
